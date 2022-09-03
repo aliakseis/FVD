cd044d7495f35ceef883c544f839d739160fd098
aliakseis <aliaksei.sanko@gmail.com>
2022-09-03 20:26:07 +0300

    just in case

--- src/common/player/opengldisplay.cpp
+++ src/common/player/opengldisplay.cpp
@@ -106,28 +106,25 @@ void OpenGLDisplay::initializeGL()
 
     // Fragment shader source code
 
-#ifdef QT_OPENGL_ES_2
-    const char* fsrc =
-        "precision mediump float; \
+    const char *fsrc = (context()->isOpenGLES())
+    ? "precision mediump float; \
     varying vec2 textureOut; \
-        uniform sampler2D tex_y; \
-        uniform sampler2D tex_u; \
-        uniform sampler2D tex_v; \
-        void main(void) \
-        { \
-            vec3 yuv; \
-            vec3 rgb; \
-            yuv.x = texture2D(tex_y, textureOut).r; \
-            yuv.y = texture2D(tex_u, textureOut).r - 0.5; \
-            yuv.z = texture2D(tex_v, textureOut).r - 0.5; \
-            rgb = mat3( 1,       1,         1, \
-                        0,       -0.39465,  2.03211, \
-                        1.13983, -0.58060,  0) * yuv; \
-            gl_FragColor = vec4(rgb, 1); \
-        }";
-#else
-    const char* fsrc =
-        "varying vec2 textureOut; \
+    uniform sampler2D tex_y; \
+    uniform sampler2D tex_u; \
+    uniform sampler2D tex_v; \
+    void main(void) \
+    { \
+        vec3 yuv; \
+        vec3 rgb; \
+        yuv.x = texture2D(tex_y, textureOut).r; \
+        yuv.y = texture2D(tex_u, textureOut).r - 0.5; \
+        yuv.z = texture2D(tex_v, textureOut).r - 0.5; \
+        rgb = mat3( 1,       1,         1, \
+                    0,       -0.39465,  2.03211, \
+                    1.13983, -0.58060,  0) * yuv; \
+        gl_FragColor = vec4(rgb, 1); \
+    }"
+    : "varying vec2 textureOut; \
     uniform sampler2D tex_y; \
     uniform sampler2D tex_u; \
     uniform sampler2D tex_v; \
@@ -143,7 +140,6 @@ void OpenGLDisplay::initializeGL()
                     1.13983, -0.58060,  0) * yuv; \
         gl_FragColor = vec4(rgb, 1); \
     }";
-#endif
 
     bCompile = impl->mFShader->compileSourceCode(fsrc);
     if (!bCompile)
