b89510b560f5e1bebe84a740bea774511dd34d65
aliakseis <aliaksei.sanko@gmail.com>
2026-08-31 19:44:56 +0300

    initial revision

--- src/common/download/idownloader.h
+++ src/common/download/idownloader.h
@@ -4,13 +4,16 @@
 #include "utilities/credsretriever.h"
 
 #include <QNetworkAccessManager>
+#include <QList>
+#include <QUrl>
 
-
-/// \class DownloaderObserverInterface Observer interface for the Downloader
+/// \class DownloaderObserverInterface
+/// \brief Observer interface for the Downloader.
 struct DownloaderObserverInterface
 {
     virtual void onFinished() = 0;
-    virtual void onError(utilities::ErrorCode::ERROR_CODES code, const QString& description) = 0;
+    virtual void onError(utilities::ErrorCode::ERROR_CODES code,
+        const QString& description) = 0;
     virtual void onProgress(qint64 bytes_downloaded) = 0;
     virtual void onSpeed(qint64 bytes_per_second) = 0;
     virtual void onFileCreated(const QString& filename) = 0;
@@ -18,29 +21,49 @@ struct DownloaderObserverInterface
     virtual void onReplyInvalidated() = 0;
     virtual void onFileToBeReleased(const QString& filename) = 0;
     virtual void onStart(const QByteArray& data) = 0;
-}; // DownloaderObserverInterface
+
+    virtual ~DownloaderObserverInterface() = default;
+};
 
 struct IDownloader
 {
-    /// \enum    DuplicateDownloadNamePolicy
-    /// \brief    kReplaceFile does not change name;
-    ///         kGenerateNewName downloads to "filename(<N+1>).ext", where N=0..MAX_INT, until file exists;
-    ///
-    enum DuplicateDownloadNamePolicy { kGenerateNewName, kReplaceFile };
+    enum DuplicateDownloadNamePolicy
+    {
+        kGenerateNewName,
+        kReplaceFile
+    };
+
+    virtual ~IDownloader() = default;
 
     virtual const QString& destinationPath() const = 0;
     virtual bool setDestinationPath(const QString& destination_path) = 0;
+
     virtual qint64 totalFileSize() const = 0;
     virtual void setTotalFileSize(qint64 value) = 0;
     virtual void setExpectedFileSize(qint64 expected_size) = 0;
+
     virtual int speedLimit() const = 0;
     virtual void setSpeedLimit(int) = 0;
-    virtual void setDownloadNamePolicy(DuplicateDownloadNamePolicy download_name_policy) = 0;
 
-    virtual void Start(const QUrl& url, QNetworkAccessManager* network_manager, const QString& filename = QString(), const QStringList& httpHeaders = QStringList()) = 0;
+    virtual void setDownloadNamePolicy(
+        DuplicateDownloadNamePolicy download_name_policy) = 0;
+
+    virtual void Start(
+        const QList<QUrl>& urls,
+        QNetworkAccessManager* network_manager,
+        const QString& filename = QString(),
+        const QStringList& httpHeaders = QStringList()) = 0;
+
     virtual void Pause() = 0;
-    virtual void Resume(const QUrl& url, QNetworkAccessManager* network_manager, const QString& filename = QString(), const QStringList& httpHeaders = QStringList()) = 0;
+
+    virtual void Resume(
+        const QList<QUrl>& urls,
+        QNetworkAccessManager* network_manager,
+        const QString& filename = QString(),
+        const QStringList& httpHeaders = QStringList()) = 0;
+
     virtual void Stop() = 0;
 
-    virtual void setObserver(DownloaderObserverInterface* observer) = 0;
+    virtual void setObserver(
+        DownloaderObserverInterface* observer) = 0;
 };
